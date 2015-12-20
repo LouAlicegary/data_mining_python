76c0e51bd55cd121b7c91de9ef2d6afe90188c40
Lou Alicegary <lou@ginkgotree.com>
2015-12-20 17:58:42 -0500

    Python files through page 2-34

--- chapter_02.py
+++ chapter_02.py
@@ -1,3 +1,5 @@
+from math import sqrt 
+
 users = {
     "Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
     "Bill": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
@@ -40,6 +42,32 @@ def minkowski(rating1, rating2, r):
  
   return pow(distance, 1/r)
 
+
+def pearson(rating1, rating2):
+  
+  sum_xi_yi = 0
+  sum_xi    = 0
+  sum_yi    = 0
+  sum_x_2   = 0
+  sum_y_2   = 0
+  n         = 0
+  
+  for key in rating1:
+    if key in rating2:
+      n         += 1
+      sum_xi    += rating1[key]
+      sum_yi    += rating2[key]
+      sum_x_2   += (rating1[key] ** 2)
+      sum_y_2   += (rating2[key] ** 2)
+      sum_xi_yi += (rating1[key] * rating2[key])
+
+  if n == 0:
+    return 0
+  else:
+    covar     = sum_xi_yi - (sum_xi * sum_yi / n)
+    std_dev_x = sqrt(sum_x_2 - ((sum_xi ** 2) / n))
+    std_dev_y = sqrt(sum_y_2 - ((sum_yi ** 2) / n))
+    return covar / (std_dev_x * std_dev_y)
 
 
 # Creates a sorted list of users based on their distance to username
@@ -96,3 +124,12 @@ print "Recommended artists for Hailey are: \n %s" % recommend('Hailey', users)
 print "Recommended artists for Chan are: \n %s" % recommend('Chan', users)
 print "Recommended artists for Sam are: \n %s" % recommend('Sam', users)
 print "Recommended artists for Angelica are: \n %s" % recommend('Angelica', users)
+
+print "Pearson coefficient between Angelica and Bill is %.3f" % pearson(users['Angelica'], users['Bill']) 
+print "Pearson coefficient between Angelica and Hailey is %.3f" % pearson(users['Angelica'], users['Hailey']) 
+print "Pearson coefficient between Angelica and Jordyn is %.3f" % pearson(users['Angelica'], users['Jordyn']) 
+
+
+
+
+
